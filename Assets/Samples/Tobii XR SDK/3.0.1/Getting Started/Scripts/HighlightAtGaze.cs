cc272c9a215f7d8e1fcd75f6b926f353b2d1ba75
williagt <williagt@stud.ntnu.no>
2023-02-01 18:03:36 +0100

    Modified eye tracking test script to send LSL stream

--- Assets/Samples/Tobii XR SDK/3.0.1/Getting Started/Scripts/HighlightAtGaze.cs
+++ Assets/Samples/Tobii XR SDK/3.0.1/Getting Started/Scripts/HighlightAtGaze.cs
@@ -1,20 +1,33 @@
 ﻿// Copyright © 2018 – Property of Tobii AB (publ) - All Rights Reserved
 
+using System.Collections;
+using System.Collections.Generic;
 using Tobii.G2OM;
+using LSL4Unity.Utils;
 using UnityEngine;
 
 namespace Tobii.XR.Examples.GettingStarted
 {
     //Monobehaviour which implements the "IGazeFocusable" interface, meaning it will be called on when the object receives focus
-    public class HighlightAtGaze : MonoBehaviour, IGazeFocusable
+    public class HighlightAtGaze : ACharOutlet, IGazeFocusable
     {
         private static readonly int _baseColor = Shader.PropertyToID("_BaseColor");
         public Color highlightColor = Color.red;
+        public string channelName;
         public float animationTime = 0.1f;
 
         private Renderer _renderer;
         private Color _originalColor;
         private Color _targetColor;
+
+        public override List<string> ChannelNames
+        {
+            get
+            {
+                List<string> chanNames = new List<string> { channelName };
+                return chanNames;
+            }
+        }
 
         //The method of the "IGazeFocusable" interface, which will be called when this object receives or loses focus
         public void GazeFocusChanged(bool hasFocus)
@@ -31,24 +44,31 @@ namespace Tobii.XR.Examples.GettingStarted
             }
         }
 
-        private void Start()
+        protected override void Start()
         {
+            base.Start();
+
             _renderer = GetComponent<Renderer>();
             _originalColor = _renderer.material.color;
             _targetColor = _originalColor;
         }
 
-        private void Update()
+        protected override bool BuildSample()
         {
             //This lerp will fade the color of the object
             if (_renderer.material.HasProperty(_baseColor)) // new rendering pipeline (lightweight, hd, universal...)
             {
                 _renderer.material.SetColor(_baseColor, Color.Lerp(_renderer.material.GetColor(_baseColor), _targetColor, Time.deltaTime * (1 / animationTime)));
+                sample[0] = 'a';
+                return true;
             }
             else // old standard rendering pipline
             {
                 _renderer.material.color = Color.Lerp(_renderer.material.color, _targetColor, Time.deltaTime * (1 / animationTime));
+                sample[0] = 'b';
+                return true;
             }
+            return false;
         }
     }
 }
