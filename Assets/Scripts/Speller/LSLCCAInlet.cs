8b63254653442ac28b2c6f0eb198af53c6ed292f
William G. Tresselt <williagt@stud.ntnu.no>
2023-04-28 09:15:28 +0200

    Added comment about displaying results for spelling test

--- Assets/Scripts/Speller/LSLCCAInlet.cs
+++ Assets/Scripts/Speller/LSLCCAInlet.cs
@@ -14,6 +14,10 @@ public class LSLCCAInlet : AFloatInlet
 {
     private GameObject cluster = null; //The cluster which was last looked at and cointains the right letters
     public TextMeshProUGUI TMPResultString; //The result of the spelling is put here 
+    private string correctString = "I LIVE IN NORWAY.";
+    private int[] correctValuesCount = new int[] { 0, 0, 0, 0, 0, 0 };
+    private int[] correctValues = new int[] { 1, 3, 2, 1, 5, 0, 3, 1, 5, 3, 5, 0, 1, 1, 2, 1, 4 };
+    private int counter = 0; //Max value is correctString.Length - 1
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +41,7 @@ public class LSLCCAInlet : AFloatInlet
         //Logic for spelling words
         if (letter.Contains("<") || letter.Contains("-") && TMPResultString.text.Length > 0) //Backspace, needs text to have more than 0 letters
         {
-            TMPResultString.text.Remove(TMPResultString.text.Length);
+            TMPResultString.text = TMPResultString.text.Substring(0, TMPResultString.text.Length - 1);
         }
         else if (letter.Equals("_")) //Space
         {
@@ -47,6 +51,32 @@ public class LSLCCAInlet : AFloatInlet
         {
             TMPResultString.text += letter;
         }
+
+        //Below is result for test when spellling contents of correctString, NB very hard coded
+        string corS = "";
+
+        if (counter > correctString.Length - 1)
+        {
+
+            foreach (int corr in correctValuesCount)
+            {
+                corS += corr + " ";
+            }
+            Debug.Log("Amount of correct total: " + corS);
+            return;
+        }
+
+        if (correctValues[counter] == pos) {
+            correctValuesCount[pos] += 1;
+        }
+        
+        counter++;
+
+        foreach (int corr in correctValuesCount) {
+            corS += corr + " ";
+        }
+        Debug.Log("Amount of correct so far: " + corS);
+
     }
 
     //Setting a new cluster. Used for knowing which letter is being added to the 
